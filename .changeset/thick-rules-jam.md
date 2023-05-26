5bfe924267d25c5bd4adf0ac4749b869eda0b858
Jakob Ankarhem <jakob.ankarhem@norce.io>
2023-05-26 10:46:26 +0200

    externalize zod

---
+++ .changeset/thick-rules-jam.md
@@ -0,0 +1,6 @@
+---
+"@twistandtango/summary": patch
+"@twistandtango/cart": patch
+---
+
+externalize zod
--- cart/vite.config.ts
+++ cart/vite.config.ts
@@ -1,35 +1,38 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import livereload from 'rollup-plugin-livereload';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import livereload from "rollup-plugin-livereload";
+import pkg from "./package.json";
 
-const DEVELOPMENT = process.env.DEVELOPMENT === 'true';
+const DEVELOPMENT =
+  process.env.DEVELOPMENT && process.env.DEVELOPMENT === "true";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  mode: DEVELOPMENT ? 'development' : 'production',
+  mode: DEVELOPMENT ? "development" : "production",
   plugins: [
     svelte({
       emitCss: false,
     }),
   ],
   build: {
-    minify: DEVELOPMENT,
+    minify: !DEVELOPMENT,
     rollupOptions: {
       plugins: [
         DEVELOPMENT &&
           livereload({
-            clientUrl: 'http://localhost:35729/livereload.js?snipver=1',
-            watch: 'dist',
+            clientUrl: "http://localhost:35729/livereload.js?snipver=1",
+            watch: "dist",
             delay: 0,
           }),
       ],
-      input: './src/main.ts',
-      preserveEntrySignatures: 'strict',
+      external: [...Object.keys(pkg.peerDependencies)],
+      input: "./src/main.ts",
+      preserveEntrySignatures: "strict",
       output: {
-        entryFileNames: '[name].js',
-        format: 'systemjs',
+        entryFileNames: "[name].js",
+        format: "systemjs",
         name: null, // ensure anonymous System.register
-        dir: 'dist',
+        dir: "dist",
       },
     },
   },
